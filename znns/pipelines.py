a66c2deff8fcadc24d8112bfd78115f4335c489a
Liang Guo <git@lkuo.me>
2021-03-01 01:03:28 -0500

    LG - update image downloader

--- znns/pipelines.py
+++ znns/pipelines.py
@@ -9,8 +9,12 @@ class ImagesDownloadPipeline(ImagesPipeline):
         return item['path']
 
     def get_media_requests(self, item, info):
-        logging.info(item['path'] + item['url'])
         return scrapy.Request(item['url'], headers={
             'Referer': item['referer'],
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:85.0) Gecko/20100101 Firefox/85.0',
         })
+
+    def item_completed(self, results, item, info):
+        logging.info(results)
+        return item
+
